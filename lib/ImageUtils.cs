8633296659e7b83834355e0ec5d05d6475b44891
nidal <nidalaraf@gmail.com>
2023-10-29 19:56:50 +0400

    Simplified resize image method

--- lib/ImageUtils.cs
+++ lib/ImageUtils.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Drawing2D;
+
 namespace NotSoBraveBrowser.lib
 {
     /**
@@ -12,41 +14,17 @@ namespace NotSoBraveBrowser.lib
          */
         public static Image ResizeImage(Image img, int width, int height)
         {
-            var destRect = new Rectangle(0, 0, width, height); // Create a new rectangle with the new width and height
-            var destImage = new Bitmap(width, height); // Create a new bitmap to store the resized image
+            Bitmap newImage = new(width, height); // Create a new bitmap with the new width and height
 
-            // Set the resolution of the new bitmap to the resolution of the original image
-            destImage.SetResolution(img.HorizontalResolution, img.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage)) // Create a new graphics object from the new bitmap
+            using (Graphics graphics = Graphics.FromImage(newImage)) // Create a new graphics object from the new image
             {
-                // Set the properties of the graphics object
-
-                // SourceCopy is used to prevent the image from being transparent
-                graphics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
-
-                // Set the quality of the graphics object to high quality
-                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-
-                // Set the interpolation mode of the graphics object to high quality bicubic
-                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-
-                // Set the smoothing mode of the graphics object to high quality
-                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-
-                // Set the pixel offset mode of the graphics object to high quality
-                graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
-
-                using var wrapMode = new System.Drawing.Imaging.ImageAttributes();
-
-                // Set the wrap mode of the graphics object to tile flip XY
-                wrapMode.SetWrapMode(System.Drawing.Drawing2D.WrapMode.TileFlipXY);
-
-                // Draw the image onto the new bitmap
-                graphics.DrawImage(img, destRect, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, wrapMode);
+                graphics.SmoothingMode = SmoothingMode.HighQuality; // Set the smoothing mode to high quality
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic; // Set the interpolation mode to high quality bicubic
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality; // Set the pixel offset mode to high quality
+                graphics.DrawImage(img, new Rectangle(0, 0, width, height)); // Draw the image onto the new image
             }
 
-            return destImage; // Return the resized image
+            return newImage; // Return the new image
         }
     }
 }
